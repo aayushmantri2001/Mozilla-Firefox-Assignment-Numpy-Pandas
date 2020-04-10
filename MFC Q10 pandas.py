ec1595655a965b590821e66a6e81cb73bc0acb62
aayushmantri2001 <63457929+aayushmantri2001@users.noreply.github.com>
2020-04-10 19:24:08 +0530

    Add files via upload

---
+++ MFC Q10 pandas.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+dataset = pd.read_csv("Macintosh HD⁩ ▸ ⁨Users⁩ ▸ ⁨aayushmantri⁩ ▸ ⁨Desktop⁩ ▸ Automobile_data.csv")
+dataset['company'].value_counts()
+
+
+# In[ ]:
+
+
+
+
